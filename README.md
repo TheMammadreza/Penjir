3de9825cf11ae122c937e862de34aac65547dc4a
mammadreza <mammadreza@live.com>
2022-01-24 00:33:05 +0200

    Create all classes base on API from https://dictionaryapi.dev/

---
+++ README.md
@@ -0,0 +1,4 @@
+# Penjir
+A dictionary which is friend with you to keep words in mind forever with experienced method
+
+Use a free API from https://dictionaryapi.dev/
---
+++ app/src/main/java/com/example/penjir/Models/APIResponse.java
@@ -0,0 +1,33 @@
+package com.example.penjir.Models;
+
+import java.util.List;
+
+public class APIResponse {
+    String word = "";
+    List<Phonetic> phonetics = null;
+    List<Meaning> meanings = null;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public List<Phonetic> getPhonetics() {
+        return phonetics;
+    }
+
+    public void setPhonetics(List<Phonetic> phonetics) {
+        this.phonetics = phonetics;
+    }
+
+    public List<Meaning> getMeanings() {
+        return meanings;
+    }
+
+    public void setMeanings(List<Meaning> meanings) {
+        this.meanings = meanings;
+    }
+}
